a0caeb2fbbcd8b40a1fa77e1ec6ecc99ab902c90
Maico <maicoloncomilla@gmail.com>
2021-09-22 16:20:05 -0300

    remove js files

--- src/state/models/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.models = void 0;
-var user_1 = require("./user");
-exports.models = {
-    user: user_1.user
-};
--- src/state/store.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Store = void 0;
-var core_1 = require("@rematch/core");
-var models_1 = require("./models");
-exports.Store = core_1.init({ models: models_1.models });
